fbded6ee4875e436e6f109ca5d6a04a99825ea77
MySprtlty <MySprtlty@gmail.com>
2023-10-08 19:24:00 +0900

    feat: restore context

--- kernel/task.c
+++ kernel/task.c
@@ -103,10 +103,30 @@ static __attribute__ ((naked)) void save_context(void)
 
 }
 
-#if 0
 /*restore the context of next task*/
 static __attribute__ ((naked)) void restore_context(void)
 {
+    /*r0 = pointer to pointer to next_tcb*/
+    __asm__ ("LDR r0, =next_tcb");
 
+    /*r0 = pointer to next_tcb*/
+    __asm__ ("LDR r0, [r0]");
+
+    /*restore the stack pointer*/
+    __asm__ ("LDMIA r0!, {sp}");
+
+    /*pop cpsr*/
+    __asm__ ("POP {r0}");
+
+    /*restore cpsr*/
+    __asm__ ("MSR cpsr, r0");
+
+    /*restore GPR*/
+    __asm__ ("POP {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12}");
+
+    /*restore program counter*/
+    /*
+    program counter changes immediately
+    */
+    __asm__ ("POP {pc}");
 }
-#endif
