e363a93e160d4e8ed31cac4382e51422acc80e3b
MySprtlty <MySprtlty@gmail.com>
2023-10-08 22:18:49 +0900

    feat: user tasks & kernel init

--- boot/Main.c
+++ boot/Main.c
@@ -5,7 +5,12 @@
 #include "HalInterrupt.h"
 #include "stdio.h"
 
+#include "task.h"
+
 static void Hw_init(void);
+static void user_task0(void);
+static void user_task1(void);
+static void user_task2(void);
 
 void main(void)
 {
@@ -24,4 +29,40 @@ static void Hw_init(void)
     Hal_interrupt_init();
     Hal_uart_init();
     Hal_timer_init();
+}
+
+static void kernel_init(void)
+{
+    task_init();
+
+    /*create tasks*/
+    if(task_create(user_task0) == FULL_TASK_NUM)
+    {
+        my_printf("error: create user_task0");
+    }
+
+    if(task_create(user_task1) == FULL_TASK_NUM)
+    {
+        my_printf("error: create user_task1");
+    }
+
+    if(task_create(user_task2) == FULL_TASK_NUM)
+    {
+        my_printf("error: create user_task2");
+    }
+}
+
+static void user_task0(void)
+{
+    my_printf("user_task0 is running.");
+}
+
+static void user_task1(void)
+{
+    my_printf("user_task1 is running.");
+}
+
+static void user_task2(void)
+{
+    my_printf("user_task2 is running.");
 }
